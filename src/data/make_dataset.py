07a278b61ac200f12dc145b60bee47f25c2eea16
Luc Giffon <perso@lucgiffon.fr>
2021-06-11 22:15:56 +0200

    prepare list of candidates with their list at the city election

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 
-def create_id_from_row(row, cols_names_for_id, col_name_for_label):
+def create_id_from_row(row, cols_names_for_id, dct_col_names_as_is):
     """
     Function to be applied row-wise on a dataframe to produce the pairs (id_candidate, party)
 
@@ -24,34 +24,74 @@ def create_id_from_row(row, cols_names_for_id, col_name_for_label):
     id_candidate = "_".join(map(lambda x: str.lower(unidecode.unidecode(str(row[x]))),
                                 cols_names_for_id))
 
-    return {"id_candidate": id_candidate,
-            "party": row[col_name_for_label]}
+    dct_other_cols = dict((key, row[value]) for key, value in dct_col_names_as_is.items())
+
+    return dict(id_candidate=id_candidate, **dct_other_cols)
 
 @click.command()
-# @click.argument('input_filepath', type=click.Path(exists=True))
-# @click.argument('output_filepath', type=click.Path())
 def main():
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    prepare_sanitized_list_of_candidates()
+    prepare_sanitized_list_of_lists()
+
+
+def prepare_sanitized_list_of_candidates():
+    """Write processed file of candidates with their party.
     """
     path_candidates_list = project_dir / "data/external/Leg_2017_candidatures_T1_c2.csv"
-    path_output_sanitized_candidates_list = project_dir / "data/processed/sanitized_candidates.csv"
+    # path_nuance_association = project_dir / "data/external/Leg_2017_candidatures_T1_c2_nuances.csv"
+
+    path_output_sanitized_candidates_list = project_dir / "data/interim/sanitized_candidates.csv"
     path_output_sanitized_candidates_list.parent.mkdir(parents=True, exist_ok=True)
     with open(path_candidates_list, 'rb') as candidates_file:
-        # _str = candidates_file.read()
         df_candidates = pd.read_csv(candidates_file, sep=",", skiprows=0)
+    # with open(path_nuance_association, 'rb') as nuance_file:
+    #     df_nuance_association = pd.read_csv(nuance_file, sep=",", skiprows=0)
+    #
+    # dct_nuance_association = dict(line for line in df_nuance_association.values)
 
     cols_for_id_candidates = (
         "Code du département",
         "Nom candidat",
         "Prénom candidat"
     )
-    col_for_label = "Nuance candidat"
-    df_final = df_candidates.apply(lambda x: create_id_from_row(x, cols_for_id_candidates, col_for_label), axis=1, result_type='expand')
-    assert df_final["id_candidate"].unique().size == len(df_final)  # check there is no duplicate name
-    print(df_candidates)
+    dct_col_for_label = {
+        "party_short": "Nuance candidat"
+    }
+    df_final = df_candidates.apply(lambda x: create_id_from_row(x, cols_for_id_candidates, dct_col_for_label), axis=1, result_type='expand')
+    assert df_final["id_candidate"].unique().size == len(df_final)  # assert there is no duplicate name
+    # df_final["party_long"] = df_final.apply(lambda row: dct_nuance_association[row["party_short"]], axis=1)
+    df_final.to_csv(path_output_sanitized_candidates_list, index=False)
 
-    df_final.to_csv(path_output_sanitized_candidates_list)
+
+def prepare_sanitized_list_of_lists():
+    """
+    Write processed file of the list candidates with their list.
+    :return:
+    """
+    path_candidates_list = project_dir / "data/external/livre-des-listes-et-candidats.txt"
+    path_output_sanitized_candidates_list = project_dir / "data/interim/sanitized_lists.csv"
+    path_output_sanitized_candidates_list.parent.mkdir(parents=True, exist_ok=True)
+    with open(path_candidates_list, 'rb') as list_file:
+        df_lists = pd.read_csv(list_file, sep="\t", encoding="ISO-8859-1", skiprows=2)
+
+    df_lists = df_lists[:1000]
+    cols_for_id_candidates = (
+        "Code du département",
+        "Nom candidat",
+        "Prénom candidat"
+    )
+    dct_col_for_label = {
+        "party": "Nuance Liste",
+        # "short_list_name": "Libellé abrégé liste",
+        # "long_list_name": "Libellé Etendu Liste"
+    }
+    df_final = df_lists.apply(lambda x: create_id_from_row(x, cols_for_id_candidates, dct_col_for_label), axis=1,
+                                   result_type='expand')
+    assert df_final["id_candidate"].unique().size == len(df_final)  # assert there is no duplicate name
+
+    df_final.to_csv(path_output_sanitized_candidates_list, index=False)
+
+
 
 
 if __name__ == '__main__':
