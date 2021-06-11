c556c77ffc56495fe79ae9bb323e90500be13cec
Luc Giffon <perso@lucgiffon.fr>
2021-06-11 16:57:40 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Try to make projection of political parties or people in avector space in order to enable visualisation.',
+    author='Luc Giffon',
+    license='MIT',
+)
